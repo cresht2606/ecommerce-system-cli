b9e2e3eedca8812aec44aa446e423a890722f81b
phucan08 <nguyenphucan887@gmail.com>
2025-05-10 23:25:18 +0700

    Add files via upload

--- src/operation/OrderOperation.java
+++ src/operation/OrderOperation.java
@@ -1,5 +1,40 @@
 package operation;
 
-public class OrderOperation {
+import features.Order;
+import java.util.*;
 
+public class OrderOperation {
+    private static OrderOperation instance;
+    private List<Order> orders;
+
+    private OrderOperation() {
+        orders = new ArrayList<>();
+    }
+
+    public static OrderOperation getInstance() {
+        if (instance == null) {
+            instance = new OrderOperation();
+        }
+        return instance;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public List<Order> getOrdersByUser(String userId) {
+        List<Order> result = new ArrayList<>();
+        for (Order o : orders) {
+            if (o.toString().contains(userId)) {
+                result.add(o);
+            }
+        }
+        return result;
+    }
+
+    public List<Order> getAllOrders() {
+        return orders;
+    }
 }
+
+
