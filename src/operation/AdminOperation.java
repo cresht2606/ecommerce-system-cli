c23b89ba46520a6657d73926e514c16b17fa3097
Cresht <tmduc2606@gmail.com>
2025-05-13 23:29:08 +0700

    Resume the functions (leaving empty)

--- src/operation/AdminOperation.java
+++ src/operation/AdminOperation.java
@@ -1,14 +1,8 @@
 package operation;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
-import features.Admin;
 
 public class AdminOperation {
 
     private static AdminOperation instance;
-    private static boolean isRegisteredOnce = false;
 
     /**
     * Returns the single instance of AdminOperation.
@@ -30,56 +24,6 @@ public class AdminOperation {
     **/
     
     public void registerAdmin(){
-        if(isRegisteredOnce){
-            throw new IllegalArgumentException("The Admin account has been registered before! Expected to come in User registration phase!");
-        }
-        Scanner sc = new Scanner(System.in);
-        UserOperation userOperation = UserOperation.getInstance();
-
-        String userName;
-        String userPassword;
-
-        //Obtaining username
-        while(true){
-            System.out.println("Enter Admin name (5 characters at minimum, only letters and underscores)");
-            userName = sc.nextLine();
-
-            if(userOperation.validateUsername(userName) && !userOperation.checkUsernameExist(userName)){
-                break;
-            }
-            else{
-                throw new IllegalArgumentException("Invalid or existing username! Expected to be a human!");
-            }
-        }
-
-        //Obtaining password
-        while(true){
-            System.out.println("Enter Admin password (5 characters at minimum, must contain both letters and numbers)");
-            userPassword = sc.nextLine();
-
-            if(userOperation.validatePassword(userPassword)){
-                break;
-            }
-            else{
-                throw new IllegalArgumentException("Invalid password input! Expected to follow the principles!");
-            }
-        }
-
-        //Set Admin ID
-        String userId = userOperation.generateUniqueUserId();
-        
-        //Set format date
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");
-        String currentTime = sdf.format(new Date());
-
-        //Assign Admin information
-        Admin admin = new Admin(userId, userName, userPassword, currentTime, "admin");
-
-        //Add admin to the userlist
-        userOperation.addUser(admin);
-
-        isRegisteredOnce = true;
-        System.out.println("Admin account successfully created!");
     }
 
 }
