1778e78f5f4a2165c75d1fe1bd4bf2105a6626dd
Cresht <tmduc2606@gmail.com>
2025-05-15 21:45:02 +0700

    Update Customer operations with delete customers, update information

--- src/operation/CustomerOperation.java
+++ src/operation/CustomerOperation.java
@@ -1,15 +1,17 @@
 package operation;
 
 import features.Customer;
-import java.util.regex.*;
-
 import features.User;
 
+import java.util.regex.*;
+import java.io.BufferedReader;
 //Save .txt files
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileReader;
 //Set current time of shopping
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -63,11 +65,13 @@ public class CustomerOperation {
         if(userMobile.length() != 10){
             return false;
         }
-        if(userMobile.charAt(0) != '0'){
+        if(!userMobile.startsWith("03") && !userMobile.startsWith("04")){
             return false;
         }
-        if(userMobile.charAt(1) != '3' && userMobile.charAt(1) != '4'){
-            return false;
+        for(char c : userMobile.toCharArray()){
+            if(!Character.isDigit(c)){
+                return false;
+            }
         }
         return true;
     }  
@@ -145,6 +149,35 @@ public class CustomerOperation {
     * @return true if updated, false if failed
     */
     public boolean updateProfile(String attributeName, String value, Customer customerObject){
+        UserOperation userOp = UserOperation.getInstance();
+        if("userName".equalsIgnoreCase(attributeName)){
+            if(!userOp.validateUsername(value)){
+                return false;
+            }
+            customerObject.setUserName(value);
+            return true;
+        }
+        else if("userPassword".equalsIgnoreCase(attributeName)){
+            if(!userOp.validatePassword(value)){
+                return false;
+            }
+            customerObject.setUserPassword(value);
+            return true;
+        }
+        else if("userEmail".equalsIgnoreCase(attributeName)){
+            if(!validateEmail(value)){
+                return false;
+            }
+            customerObject.setUserEmail(value);
+            return true;
+        }
+        else if("userMobile".equalsIgnoreCase(attributeName)){
+            if(!validateMobile(value)){
+                return false;
+            }
+            customerObject.setUserMobile(value);
+            return true;
+        }
         return false;
     }
 
@@ -155,7 +188,52 @@ public class CustomerOperation {
     * @return true if deleted, false if failed
     */
     public boolean deleteCustomer(String customerId){
-        return false;
+        File inputFile = new File("ecommerce-system-cli/database/users.txt");
+        File tempFile = new File("ecommerce-system-cli/database/backup_users.txt");
+        boolean deleted = false;
+
+        try(
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))
+        ){
+            String currentLine;
+
+            while((currentLine = reader.readLine()) != null){
+                String extractedId = extractUserId(currentLine);
+                if(customerId.equals(extractedId)){
+                    deleted = true;
+                    continue;
+                }
+                writer.write(currentLine);
+                writer.newLine();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
+        if(deleted){
+            if(!inputFile.delete() || !tempFile.renameTo(inputFile)){
+                return false;
+            }
+        } else{
+            tempFile.delete();
+        }
+        return deleted;
+    }
+
+    //Helper extract customerID
+    public String extractUserId(String dataline){
+        String key = "\"user_id\":\"";
+        int start = dataline.indexOf(key);
+        if(start == -1){
+            return null;
+        }
+        start += key.length();
+        int end = dataline.indexOf("\"", start);
+        if(end == -1){
+            return null;
+        }
+        return dataline.substring(start, end);
     }
 
     /**
@@ -172,8 +250,13 @@ public class CustomerOperation {
     /**
     * Removes all the customers from the data/users.txt file.
     */
-    public void deleteAllCustomers() {
-    }
 
+    public void deleteAllCustomers() {
+        try(PrintWriter writer = new PrintWriter("ecommerce-system-cli/database/users.txt")){
+            //This will leave the blank space on users.txt
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
 }
