9c70c011611549862c41c2f5f9557161329f3c2e
Cresht <tmduc2606@gmail.com>
2025-05-16 19:52:12 +0700

    Refined Product & Order Class

--- src/features/Order.java
+++ src/features/Order.java
@@ -6,6 +6,7 @@ public class Order {
     private String proId;
     private String orderTime;
 
+    //Constructor
     public Order(String orderId, String userId, String proId, String orderTime) {
         this.orderId = orderId;
         this.userId = userId;
@@ -13,14 +14,47 @@ public class Order {
         this.orderTime = orderTime;
     }
 
-    public String getOrderId() { return orderId; }
-    public String getUserId() { return userId; }
-    public String getProId() { return proId; }
-    public String getOrderTime() { return orderTime; }
+    //Default constructor
+    public Order(){
+        this.orderId = "";
+        this.userId = "";
+        this.proId = "";
+        this.orderTime = "";
+    }
+
+    //Getters
+    public String getOrderId() {
+        return orderId;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public String getProId() {
+        return proId;
+    }
+    public String getOrderTime() {
+        return orderTime;
+    }
+
+    
+    //Setters
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public void setProId(String proId) {
+        this.proId = proId;
+    }
+    public void setOrderTime(String orderTime) {
+        this.orderTime = orderTime;
+    }
+
 
     @Override
     public String toString() {
-        return String.format("{\"orderId\":\"%s\", \"userId\":\"%s\", \"proId\":\"%s\", \"orderTime\":\"%s\"}",
+        return String.format("{\"order_id\":\"%s\", \"user_id\":\"%s\", \"pro_id\":\"%s\", \"order_time\":\"%s\"}",
                 orderId, userId, proId, orderTime);
     }
 }
--- src/features/Product.java
+++ src/features/Product.java
@@ -10,6 +10,7 @@ public class Product {
     private double proDiscount;
     private int proLikeCount;
 
+    //Constructor
     public Product(String proId, String proName, String proCategory, String proModel,
                    double proCurrentPrice, double proRawPrice, double proDiscount, int proLikeCount) {
         this.proId = proId;
@@ -22,15 +23,81 @@ public class Product {
         this.proLikeCount = proLikeCount;
     }
 
-    public String getProId() { return proId; }
-    public String getProName() { return proName; }
-    public String getProCategory() { return proCategory; }
-    public String getProModel() { return proModel; }
-    public double getProCurrentPrice() { return proCurrentPrice; }
+    //Default Constructor
+    public Product(){
+        this.proId = "";
+        this.proName = "";
+        this.proCategory = "";
+        this.proModel = "";
+        this.proCurrentPrice = 0.0;
+        this.proRawPrice = 0.0;
+        this.proDiscount = 0.0;
+        this.proLikeCount = 0;
+
+    }
+
+
+    //Setters
+    public void setProId(String proId) {
+        this.proId = proId;
+    }
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+    public void setProCategory(String proCategory) {
+        this.proCategory = proCategory;
+    }
+    public void setProModel(String proModel) {
+        this.proModel = proModel;
+    }
+    public void setProCurrentPrice(double proCurrentPrice) {
+        this.proCurrentPrice = proCurrentPrice;
+    }
+    public void setProRawPrice(double proRawPrice) {
+        this.proRawPrice = proRawPrice;
+    }
+    public void setProDiscount(double proDiscount) {
+        this.proDiscount = proDiscount;
+    }
+    public void setProLikeCount(int proLikeCount) {
+        this.proLikeCount = proLikeCount;
+    }
+
+    
+    //Getters
+    public String getProId() {
+        return proId;
+    }
+    public String getProName() {
+        return proName;
+    }
+    public String getProCategory() {
+        return proCategory;
+    }
+    public String getProModel() {
+        return proModel;
+    }
+    public double getProCurrentPrice() {
+        return proCurrentPrice;
+    }
+    public double getProRawPrice() {
+        return proRawPrice;
+    }
+    public double getProDiscount() {
+        return proDiscount;
+    }
+    public int getProLikeCount() {
+        return proLikeCount;
+    }
 
     @Override
     public String toString() {
-        return String.format("{\"proId\":\"%s\", \"name\":\"%s\", \"price\":%.2f, \"like\":%d}",
-                proId, proName, proCurrentPrice, proLikeCount);
+        return String.format(
+            "{\"pro_id\":\"%s\", \"pro_model\":\"%s\", \"pro_category\":\"%s\", " +
+            "\"pro_name\":\"%s\", \"pro_current_price\":\"%.2f\", \"pro_raw_price\":\"%.2f\", " +
+            "\"pro_discount\":\"%.2f\", \"pro_likes_count\":\"%d\"}",
+            proId, proModel, proCategory, proName,
+            proCurrentPrice, proRawPrice, proDiscount, proLikeCount
+        );
     }
 }
